22f2545a315ae0c1e4413dc4232d78723d2cd24c
Mudather7 <mudatherabdo00@gmail.com>
2024-02-01 06:05:55 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- *
- * @node: a pointer to the node to check
- *
- * Return: 1 if node is a leaf, otherwise 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (node->left != NULL)
-		return (0);
-	if (node->right != NULL)
-		return (0);
-	return (1);
-}
-
-/**
  * binary_tree_height - function that measures the height of a binary tree
  *
  * @tree: a pointer to the root node of the tree to measure the height.
@@ -28,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
