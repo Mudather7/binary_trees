876d802018d030d00af250926b3c29d59ca54ac4
Mudather7 <mudatherabdo00@gmail.com>
2024-02-01 04:29:38 -0800

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,46 +1,23 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- *
- * @node: a pointer to the node to check
- *
- * Return: 1 if node is a leaf, otherwise 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (node->left != NULL)
-		return (0);
-	if (node->right != NULL)
-		return (0);
-	return (1);
-}
-
-/**
  * binary_tree_depth - function that measures the depth
  * of a node in a binary tree
  *
- * @tree: a pointer to the node to measure the depth
+ * @tree: pointer to the node to measure the depth
  *
- * Return: depth of a node or 0 if the tree is null
+ * Return: depth of the node, or 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth = 0, right_depth = 0;
+	size_t depth = 0;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
-	{
+	if (tree == NULL)
 		return (0);
-	}
-	else
+
+	while (tree->parent)
 	{
-		left_depth = binary_tree_depth(tree->left);
-		right_depth = binary_tree_depth(tree->right);
-
-		if (left_depth > right_depth)
-			return (left_depth + 1);
-
-		return (right_depth + 1);
+		depth++;
+		tree = tree->parent;
 	}
+	return (depth);
 }
