66de92179c66ed3bb38764dfa0801278d8c796c0
Mudather7 <mudatherabdo00@gmail.com>
2024-02-01 05:39:39 -0800

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,7 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 		count++;
 
 	count += binary_tree_nodes(tree->left);
