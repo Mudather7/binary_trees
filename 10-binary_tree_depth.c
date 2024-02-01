214cf9ea42ddbd01b0075d7a2b68c083a54c4467
Mudather7 <mudatherabdo00@gmail.com>
2024-02-01 04:09:35 -0800

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,13 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t left_depth, right_depth;
+
 	if (tree == NULL)
 		return (0);
-	size_t left_depth = binary_tree_depth(tree->left);
-	size_t right_depth = binary_tree_depth(tree->right);
+
+	left_depth = binary_tree_depth(tree->left);
+	right_depth = binary_tree_depth(tree->right);
 
 	if (left_depth > right_depth)
 		return (left_depth + 1);
